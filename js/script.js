4f3d84ef84840ff43bf1794d4c4df93b948f3d83
mariabune <marbun01@iba.dk>
2026-05-02 15:55:02 +0530

    js færdig

--- js/script.js
+++ js/script.js
@@ -41,38 +41,60 @@ const nextStage = (e) => {
             btnsText = ["Log ind", "Tilbage", "Luk"];
             break;
 
-        case "Ignorer":                     // ----> Scene 2b (tvivl-scene)
+        case "Ignorer":                     // --> Scene 2b (tvivl-scene)
             h2Text = "Er du sikker?";
             pText = "Hvis du ignorerer beskeden, kan din SU blive stoppet."
             btnsText = ["Opdater nu", "Luk"];
             break;
 
         // Scene 2a (login-scene)
-        case "log ind":                         // ----> Scene 3a (phishing ending-scene)
+        case "log ind":                         // --> Scene 3a (phishing ending-scene)
             h2Text = "Du er blevet phishet";
             pText = "Giv aldrig dine oplysninger via links i beskeder - gå altid direkte til officielle hjemmesider";
             btnsText = ["Start forfra"];
             break;
 
-        case "Tilbage":                         // ---> Scene 1 (start-scene)
+        case "Tilbage":                         // --> Scene 1 (start-scene)
             h2Text = "Besked fra SU-kontoret";  
             pText = "Din SU er i fare. Opdater dine oplysninger nu for ikke at miste din SU næste måned";
             btnsText = ["Opdater nu", "Ignorer"];
             break;
 
-        case "Luk":    
+        case "Luk":                       // --> Scene 4a (safe endning-scene) 
+            h2Text = "Tillykke";
+            pText = "Du undgik phishing ved at lukke siden og ikke give dine oplysninger";
+            btnsText = ["Start forfra"];
+            break;
 
 
-
-
-
-        // Scene 2b (tvivl)
-        case "Opdater nu":                  // går til scene 2a (login-scene)
+        // Scene 2b (tvivl-scene)
+        case "Opdater nu":                  // --> scene 2a (login-scene)
             h2Text = "Login til SU-systemet";
-            pText = "Indtast dine oplysninger for at fortsætte."
+            pText = "Indtast dine oplysninger for at fortsætte.";
+            btnsText = ["Log ind", "Tilbage", "Luk"];
+            break;
 
+        case "Luk":                         // --> Scene 4a (safe endning-scene)
+            h2Text = "Tillykke";
+            pText = "Du undgik phishing ved at lukke siden og ikke give dine oplysninger" 
+            btnsText = [Start forfra];
+            break;
+            
+        // Start forfra - reset
+        case "Start forfra":
+            h2Text = "Besked fra SU-kontoret";
+            pText = "Din SU er i fare. Opdater dine oplysninger nu for ikke at miste din SU næste måned";
+            btnsText = ["Opdater nu", "Ignorer"];
+            break;
 
-
+            default: console.log("Don't know"); // For debugging
 
     }
+
+updateUI(h2Text, pText, btnsText); // Kalder på UI() funktion og opdaterer oplysninger
+
 };
+
+for (const btn of btns) {
+    btn.addEventListener("click", nextStage);
+}
